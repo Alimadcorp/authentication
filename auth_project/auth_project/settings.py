f530022565a77dab3d6a161fe0727ec1af68314b
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2025-08-30 22:38:08 +0500

    br

--- auth_project/auth_project/settings.py
+++ auth_project/auth_project/settings.py
@@ -123,19 +123,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-INSTALLED_APPS += [
-    'cloudinary',
-    'cloudinary_storage',
-]
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-}
-
 LOGIN_URL = '/login/'
 LOGOUT_URL = '/logout/'
 
