a914db95dcd145d1473ee2a16f3246d90ac16df7
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2025-08-30 23:04:32 +0500

    a

--- auth_project/auth_project/settings.py
+++ auth_project/auth_project/settings.py
@@ -74,10 +74,9 @@ WSGI_APPLICATION = "auth_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+        default="postgresql://postgres:VtewvRCJDaYKgtrdMQcxwANjhknsfpiS@mainline.proxy.rlwy.net:32773/railway"
     )
 }
 
